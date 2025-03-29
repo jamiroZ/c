6ff6f9b4dc786d5c203d036e2298defb5e98d0ff
jamiroZ <jamiro.zuniga@est.fi.uncoma.edu.ar>
2025-03-29 13:49:51 -0300

    ejercicio del ahorcado

---
+++ tp0/ejercicio12/ahorcado.c
@@ -0,0 +1,77 @@
+/*
+12.b Complete el código ahorcado.c usando printf() y getchar(), para 
+    desarrollar el juego del ahorcado.
+*/
+#include <stdio.h>
+#include <stdlib.h>  /* para las funciones system y exit */
+#include <string.h>
+
+int main() {
+    int c, cont, iguales;
+    cont = 0;
+    iguales = 0;
+    char palabra[] = "peliculas";  // Palabra a adivinar
+    int longitud = strlen(palabra); // Longitud de la palabra
+    char check[longitud + 1];  // Palabra vacía, con espacio para el carácter nulo
+
+    // Creo la cadena para rellenar
+    for (int i = 0; i < longitud; i++) {
+        if (i == (longitud / 2)) {  // Coloca una letra en la mitad de la cadena
+            check[i] = palabra[i];
+        } else {  // Relleno con _
+            check[i] = '_';
+        }
+    }
+    check[longitud] = '\0';  // Caracter nulo al final de la palabra
+
+    printf("\n%s\n", check);//muestro la palabra incompleta
+    printf("\n%s\n", palabra);//muestro la palabra completa
+
+    /* Decirle al sistema que el modo input es RAW */
+    system("/bin/stty raw");
+    int cantIntentos = 10;//cantidad de intentos 
+
+    while (cont < cantIntentos && iguales < longitud) {  // repite hasta que se completo la palabra o se acabaron los intentos
+        iguales = 0;  // Reseteo de iguales para la siguiente iteración
+        printf("\rIngrese una letra (0 para salir): ");
+        c = getchar();  
+
+        if (c == '0') {// Si el usuario ingresa '0', se sale del juego
+            printf("\nSaliendo del juego.\n");
+            break; 
+        }
+
+        int bandera = 0;
+        // Comparar cada letra de la palabra con el input
+        for (int pos = 0; pos < longitud; pos++) {
+            if (palabra[pos] == c && check[pos] != c) {  // Si la letra es correcta
+                check[pos] = c;  // Coloca la letra
+                bandera = 1;
+            }
+        }
+
+        if (!bandera) {  // Si no se encontró la letra en la palabra
+            cont++;  // Usar un intento
+        }
+
+        // cuántas letras han fueron descubiertas 
+        iguales = 0;
+        for (int i = 0; i < longitud; i++) {
+            if (check[i] == palabra[i]) {
+                iguales++;
+            }
+        }
+        // muestra la palabra actual 
+        printf("\n%s\n", check);
+        
+        if (cont == cantIntentos) {//se quedo sin intentos
+            printf("\nHas agotado el límite de intentos\n");
+            break;
+        } else if (iguales == longitud) {//obtuvo la palabra
+            printf("\nPalabra encontrada\n");
+            break;
+        }
+    }
+    system("/bin/stty sane erase ^H");
+    return 0;
+}
